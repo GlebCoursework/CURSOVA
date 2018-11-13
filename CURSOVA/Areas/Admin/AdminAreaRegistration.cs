ccc35b43adb2e02449292710727ce66f0fb2bb3f
Sergey Korolchuk <antinomy.rv@gmail.com>
2018-11-13 14:14:50 +0200

    Admin

--- CURSOVA/Areas/Admin/AdminAreaRegistration.cs
+++ CURSOVA/Areas/Admin/AdminAreaRegistration.cs
@@ -2,23 +2,22 @@
 
 namespace CURSOVA.Areas.Admin
 {
-    public class AdminAreaRegistration : AreaRegistration 
+    public class AdminAreaRegistration : AreaRegistration
     {
-        public override string AreaName 
+        public override string AreaName
         {
-            get 
+            get
             {
                 return "Admin";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "Admin_default",
                  "Admin/{controller}/{action}/{id}",
-               defaults: new {action = "Index", id = UrlParameter.Optional },
-               namespaces: new[] { "CURSOVA.Areas.Admin.Controllers" }
+                 new { action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- CURSOVA/Scripts/AdminScripts.js
+++ CURSOVA/Scripts/AdminScripts.js
@@ -1,3 +1,14 @@
-﻿function Model() {
+﻿function EditPizza(item) {
+    $.ajax({
 
+        url: "Admin/Home/EditPizza",
+        contentType: "application/json; charset=utf-8",
+        data: { id: item },
+        cache: false,
+        async: true,
+        success: function (result) {
+            $("#Div-Edit").html(result);
+        }
+    });
+    return false;
 }
