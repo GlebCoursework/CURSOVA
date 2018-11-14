b3205b9b92193fe61ad2d6fe4fbd4852a5b4025b
Sergey Korolchuk <antinomy.rv@gmail.com>
2018-11-14 11:12:45 +0200

    Edit user View

--- CURSOVA/Areas/Admin/Models/PizzaModelAdmin.cs
+++ CURSOVA/Areas/Admin/Models/PizzaModelAdmin.cs
@@ -6,8 +6,13 @@ using System.Web;
 
 namespace CURSOVA.Areas.Admin.Models
 {
-    public class PizzaModelAdmin : PizzaModel
+    public class PizzaModelAdmin
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public List<string> PizzasComponents { get; set; }
         public bool Missing { get; set; }
     }
 }
---
+++ CURSOVA/Areas/Admin/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CURSOVA.Areas.Admin.Models
+{
+    public class UserModel
+    {
+
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public bool Bannes { get; set; }
+        public List<string> Roles { get; set; }
+
+    }
+}
